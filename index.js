126a6cf66864c63a8b429d93a6407437ff1b5d3a
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-03 12:59:32 -0700

    function prompts user for description input and writes answer to the console

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+
+promptUser();
+
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the Title of Your Project?",
+                name: "description",
+            },
+            // {
+            //     type: "input",
+            //     message: "Where are you from?",
+            //     name: "location",
+            // },
+            // {
+            //     type: "input",
+            //     message: "Tell us about yourself:",
+            //     name: "bio",
+            // },
+            // {
+            //     type: "input",
+            //     message: "What is your github username?",
+            //     name: "github",
+            // },
+            // {
+            //     type: "input",
+            //     message: "What is your Linkedin username?",
+            //     name: "linkedin",
+            // },
+        ])
+        .then(function (answers) {
+            console.log(answers)
+        });
+    }
