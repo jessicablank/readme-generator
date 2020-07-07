a939b2f142d0cd5b725aab77cfca141648a83ccf
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-06 20:46:02 -0700

    wooo! figured out the badge situation

--- NEWREADME.md
+++ NEWREADME.md
@@ -1,7 +1,7 @@
-# Facts about my Dog
-![License](https://img.shields.io/badge/license-MIT-brightgreen)
+# 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 ## Description:  
- My dog is my favorite being so I created an app where users can ask questions and receive answers about my dog. His name is Bones.
+ 
 
     
 ## Table of Contents:
@@ -13,22 +13,23 @@
 * [License](#license-info)
 
 ## Installation Instructions
-Fork and Go! Ready to run in VS Code.
+Fork and Go! Ready to run in VS Code. 
 
 ## Usage
-Enter a question into the field and press "submit" to receive an answers. 
+
 
 ## Contributing
 Create a pull request. 
 
 ## Tests
-WHEN the user enters a question, THEN the user receives a response.
+
 
 ## Questions
-You can reach the author, Jessica Blankemeier,  via [github](http://github.com/jessicablank) and [email](mailto:jessicablankemeier@gmail.com)
-![GitHub](https://img.shields.io/github/followers/jessicablank?label=follow&style=social)
+You can reach the author, ,  via [github](http://github.com/) and [email](mailto:)
+![GitHub](https://img.shields.io/github/followers/?label=follow&style=social)
 
 ## License
-Copyright 2020 - present Jessica Blankemeier.
-This project is licensed under the terms of the MIT license. 
-More information is available at [opensource.org/licenses](http:opensource.org/licenses/MIT)
+Copyright 2020 - present .
+This project is licensed under the terms of the GNU General Public version 3 license. 
+More information is available at [opensource.org/licenses](https://www.gnu.org/licenses/gpl-3.0)
+;
--- index.js
+++ index.js
@@ -1,107 +1,93 @@
 const fs = require("fs");
-const util = require("util");
 
+//Reminder to install node modules (npm init -y) &  inquirer(npm i inquire --save)  
 const inquirer = require("inquirer");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+let licenseBadge;
+let licenseURL;
 
-main();
 
-
-//Run the Application
-async function main() {
-    try {
-        const answers = await promptUser();
-        const mdPage = renderMD(answers);
-        await writeFileAsync("NEWREADME.md", mdPage);
-        console.log('Success! Please copy your "NEWREADME.md" into github or save the file in your project folder as "README.md" ');
-    } catch (error) {
-        console.log(error);
-    }
-
-}
 //Get the current year
 let date = new Date().getFullYear();
+ 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "list",
+        message: "Select a license for your project and press 'Enter':",
+        name: "license",
+        choices: ["MIT", "GNU General Public version 3", "Apache_2.0",],
+    },
+    {
+        type: "input",
+        message: "Briefly describe your project:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Provide installation instructions for your project:",
+        name: "installation",
+        default: "Fork and Go! Ready to run in VS Code. "
+    },
+    {
+        type: "input",
+        message: "Explain how to use your project:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "How can users contribute to this project?",
+        name: "contribution",
+        default: "Create a pull request. "
+    },
+    {
+        type: "input",
+        message: "How can users test your project?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What is your first and last name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
 
-//Returns promise for user response object. 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "title",
-        },
-        {
-            type: "list",
-            message: "Select a license for your project and press 'Enter':",
-            name: "license",
-            choices: ['MIT', 'Apache_2.0', 'GNU'],
-        },
-        {
-            type: "input",
-            message: "Briefly describe your project:",
-            name: "description",
-        },
-        {
-            type: "input",
-            message: "Provide installation instructions for your project:",
-            name: "installation",
-            default: "Fork and Go! Ready to run in VS Code. "
-        },
-        {
-            type: "input",
-            message: "Explain how to use your project:",
-            name: "usage",
-        },
-        {
-            type: "input",
-            message: "How can users contribute to this project?",
-            name: "contribution",
-            default: "Create a pull request. "
-        },
-        {
-            type: "input",
-            message: "How can users test your project?",
-            name: "test",
-        },
-        {
-            type: "input",
-            message: "What is your first and last name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is your github username?",
-            name: "github",
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-        },
-    ])
-}
-//Need help here
-// const licenseURL = 
+]).then(function (answers) {
+    switch (answers.license) {
+        case "MIT":
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            licenseURL = "https://opensource.org/licenses/MIT"
+            break;
+        case "GNU General Public version 3":
+            licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            licenseURL = "https://www.gnu.org/licenses/gpl-3.0"
+            break;
+        case "Apache_2.0":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            licenseURL = "https://opensource.org/licenses/Apache-2.0"
+            break;
+        default:
+            licenseBadge = "";
+            licenseURL = "https://opensource.org/licenses"
+            break;
+    }
 
-// var licenseURL = answers.license;
-// switch (licenseURL) {
-//     case "Apache_2.0":
-//         console.log("Apache-2.0");
-//     break;
-//     case "GNU":
-//         console.log("gpl-license");
-//     break;
-//     default:
-//          "MIT";
-//     }
-//   ;
 
-//Returns markdown string given user input
-function renderMD(answers) {
-    //adjust license text for url link 
-    return mdPage = `# ${answers.title}
-![License](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+    const mdPage = `# ${answers.title}
+${licenseBadge}
 ## Description:  
  ${answers.description}
 
@@ -133,9 +119,15 @@ You can reach the author, ${answers.name},  via [github](http://github.com/${ans
 ## License
 Copyright ${date} - present ${answers.name}.
 This project is licensed under the terms of the ${answers.license} license. 
-More information is available at [opensource.org/licenses](http:opensource.org/licenses/${answers.license})
-`
-}
+More information is available at [opensource.org/licenses](${licenseURL})
+;`
 
+    fs.writeFile("NEWREADME.md", mdPage, function (error) {
+        if (error) {
+            console.log(error)
+            return console.log(error);
+        }
 
-
+        console.log('Success! Please copy your "NEWREADME.md" into github or save the file in your project folder as "README.md" ');
+    });
+});
